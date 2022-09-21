0509dd6a5b1bc735979f35a67b7693c0c6215d30
lupaes01 <107504260+lupaes01@users.noreply.github.com>
2022-09-21 08:36:37 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -13,12 +13,8 @@ st.subheader("Conheça-nos melhor!")
 st.write("Nós somos uma plataforma que disponibiliza funções para os gestores das empresas distribuírem as tarefas entre os colaboradores de maneira mais assertiva. Já os colaboradores, podem ver se estão sobrecarregados, a partir de uma maneira tecnológica de identificação. Além disso, disponibilizamos acesso de melhoria de qualidade de vida aos colaboradores, podem agendar sessões com terapeutas, psicólogos e selecionar funções para relaxar, como meditações.")
 
 x = st.slider('Escolha o seu nível de ansiedade devido ao trabalho:')
-if x < 50 :
 if x <= 50 :
   st.write ("Você não está ansioso")
-if x > 50 & x < 80 :
-  st.write ("Você está ficando ansioso")
-if x > 80 :
 if x >= 80 :
   st.write ("Você está muito ansioso. Consulte um psicólogo!")
 else :
